7025f2db7cd08b50e312f279f97216be9e97cd85
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-13 18:15:41 +0300

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   let brackets = [];
+  let tmp1 = 0;
+  let tmp2 = 0;
   for(let i = 0; i < bracketsConfig.length; i++){
     brackets[i] = 0;
   }
@@ -13,6 +15,17 @@ module.exports = function check(str, bracketsConfig) {
         if(brackets[j] == -1) return false;
       }
     }
+  }
+  for(let i = 0; i < str.length - 1; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+      if(str[i] == bracketsConfig[j][0])
+      tmp1 = j;
+    }
+    for(let j = 0; j < bracketsConfig.length; j++){
+      if(str[i+1] == bracketsConfig[j][1])
+      tmp2 = j;
+    }
+    if (tmp1 != tmp2) return false;
   }
   return true;
 }
