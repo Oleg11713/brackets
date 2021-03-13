067fa83afa2fe3254b4590b06dc5a08c72e942fa
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-13 18:56:37 +0300

    fix: really the last fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   let brackets = [];
-  let tmp1 = 0;
-  let tmp2 = 0;
+  let tmp1;
+  let tmp2;
   for(let i = 0; i < bracketsConfig.length; i++){
     brackets[i] = 0;
   }
@@ -9,25 +9,14 @@ module.exports = function check(str, bracketsConfig) {
     for(let j = 0; j < bracketsConfig.length; j++){
       if(str[i] == bracketsConfig[j][0]){
         brackets[j]++;
-      }
-      if(str[i] == bracketsConfig[j][1]){
+      }if(str[i] == bracketsConfig[j][1]){
         brackets[j]--;
         if(brackets[j] == -1) return false;
       }
     }
   }
-  for(let i = 0; i < str.length - 1; i++){
-    tmp1 = tmp2 = -1;
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i] == bracketsConfig[j][0])
-      tmp1 = j;
-    }
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i+1] == bracketsConfig[j][1])
-      tmp2 = j;
-    }
-    if(tmp1 == -1 || tmp2 == -1) continue;
-    if (tmp1 != tmp2) return false;
+  for(let i = 0; i < brackets.length; i++){
+    if(brackets[i] % 2 != 0) return false;
   }
   return true;
 }
