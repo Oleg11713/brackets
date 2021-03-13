a13914e610b8df4e270b0cea12522e1e613698e0
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-13 19:25:51 +0300

    fix: fix fix and one more time fix

--- src/index.js
+++ src/index.js
@@ -1,35 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = [];
-  let tmp1;
-  let tmp2;
-  for(let i = 0; i < bracketsConfig.length; i++){
-    brackets[i] = 0;
+  let array = [];
+  let map = new Map();
+  for (const iter of bracketsConfig) {
+    map.set(iter[0], iter[1]);
   }
-  for(let i = 0; i < str.length; i++){
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i] == bracketsConfig[j][0]){
-        brackets[j]++;
-      }if(str[i] == bracketsConfig[j][1]){
-        brackets[j]--;
-        if(brackets[j] == -1) return false;
-      }
-    }
+  array.push(str[0]);
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === map.get(array[array.length - 1])) array.pop(); 
+    else array.push(str[i]);
   }
-  for(let i = 0; i < brackets.length; i++){
-    if(brackets[i] % 2 != 0) return false;
-  }
-  for(let i = 0; i < str.length - 1; i++){
-    tmp1 = tmp2 = -1;
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i] == bracketsConfig[j][0])
-      tmp1 = j;
-    }
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i+1] == bracketsConfig[j][1])
-      tmp2 = j;
-    }
-    if(tmp1 == -1 || tmp2 == -1) continue;
-    if (tmp1 != tmp2) return false;
-  }
+  if (array.length > 0) return false;
   return true;
 }
