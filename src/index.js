cdd262b5235994891acc93d18385071d9794eff8
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-13 17:48:36 +0300

    feat: add body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  brackets = [];
+  for(let i = 0; i < bracketsConfig.length; i++);{
+    brackets[i] = 0;
+  }
+  for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < bracketsConfig.length; i++){
+      if(str[i] == bracketsConfig[j][0]){
+        brackets[j]++;
+      }
+      if(str[i] == bracketsConfig[j][1]){
+        brackets[j]--;
+        if(brackets[j] == -1) return false;
+      }
+    }
+  }
+  return true;
 }
